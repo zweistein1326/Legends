458d0f6e94bd6233005118bb8a27a341cc3b5182
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2025-04-08 00:33:31 +0800

    update hero section h1 and h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,36 @@
 "use client";
-import { ElevenLabsClient } from "elevenlabs";
-import { GoogleGenAI } from "@google/genai";
+import { ElevenLabsClient, play } from "elevenlabs";
+// import { GoogleGenAI } from "@google/genai";
 
 const speakQuote = async (text: string) => {
   const client = new ElevenLabsClient({
     apiKey: "sk_a887a06b8b4061bc386fede4e6c7aaa20337b9c82a8e70ac",
   });
-  console.log(text.toString());
-  await client.textToSpeech.convert("JBFqnCBsd6RMkjVDRZzb", {
-    output_format: "mp3_44100_128",
+  const audio = await client.textToSpeech.convert("JBFqnCBsd6RMkjVDRZzb", {
     text: text.toString(),
     model_id: "eleven_multilingual_v2",
+    output_format: "mp3_44100_128",
   });
-  // console.log(blob);
+  await play(audio);
 };
 
+const dummyResponse =
+  "Sid, at 25, you're writing your own story. Every cigarette you resist is a powerful stroke, painting a future of strength, health, and limitless possibilities. This is your chapter. Make it a masterpiece. You got this!";
+
 const generateQuote = async () => {
-  const ai = new GoogleGenAI({
-    apiKey: "AIzaSyCIJNNtxSBVQoy4ouf5nnAm413iG8X3whA",
-  });
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents:
-      "Generate a motivational quote for Sid, a 25yo male who wants to quit his bad habit of smoking cigarettes.",
-  });
-  if (response.text) {
-    speakQuote(response.text);
+  // const ai = new GoogleGenAI({
+  //   apiKey: "AIzaSyCIJNNtxSBVQoy4ouf5nnAm413iG8X3whA",
+  // });
+  // const response = await ai.models.generateContent({
+  //   model: "gemini-2.0-flash",
+  //   contents:
+  //     "Generate a motivational quote for Sid, a 25yo male who wants to quit his bad habit of smoking cigarettes.",
+  // });
+  // if (response.text) {
+  //   speakQuote(response.text);
+  // }
+  if (dummyResponse) {
+    speakQuote(dummyResponse);
   }
 };
 
